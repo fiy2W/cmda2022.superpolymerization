a37d27da4a068a5818bf8a1e40aa430baca76b77
fiy2W <13678126924@163.com>
2022-08-20 22:41:28 +0800

    only use cuda:0

--- docker/inference/run_inference.py
+++ docker/inference/run_inference.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import numpy as np
 import SimpleITK as sitk
 import shutil
